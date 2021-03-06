1651a2ada60d8433fce72d5f94d4bf52c9142c79
Kana Otawara <shimmer22.0204@gmail.com>
2021-03-06 21:01:24 +0900

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent }     from './app.component';
 import { AngularComponent } from './angular.component';
 import { PageComponent } from './page/page.component';
 import { HeaderComponent } from './page/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './page/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
