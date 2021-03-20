38f510458f0aacea468798aa4f78ac582fe1c717
Kana Otawara <shimmer22.0204@gmail.com>
2021-03-21 02:19:03 +0900

    enable selection of date & time

--- src/app/page/modal/event-edit/event-edit.component.ts
+++ src/app/page/modal/event-edit/event-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA }           from '@angular/material/dialog';
 import { FormControl, FormGroup }    from '@angular/forms';
 import * as dayjs                    from 'dayjs';
+import * as duration                 from 'dayjs/plugin/duration';
 
 export type EventEditDialogData = {
   date: dayjs.Dayjs,
@@ -19,20 +20,40 @@ export class EventEditComponent implements OnInit {
 
   isCalendarOpen = false
 
+  isTimePickerOpen = false
+
+  get date(): dayjs.Dayjs {
+    return this.form.value.date.format('M月D日')
+  }
+
+  get time(): dayjs.Dayjs {
+    return this.form.value.time.format('mm:ss')
+  }
+
+  timeOptions: dayjs.Dayjs[]
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: EventEditDialogData
   ) {
+    dayjs.extend(duration)
     this.form = new FormGroup({
       title: new FormControl(''),
       date: new FormControl(this.data.date),
+      time: new FormControl(dayjs.duration(0, 'minutes')),
     })
   }
 
   ngOnInit(): void {
+    const result = []
+    for (let minutes = 0; minutes < 60 * 24 / 15; minutes++) {
+      result.push(dayjs.duration(0, 'minutes').add(minutes * 15, 'minutes'))
+    }
+    this.timeOptions = result
   }
 
   onChangeActiveDate($event: dayjs.Dayjs): void {
     this.form.patchValue({ date: $event })
+    this.isCalendarOpen = false
   }
 
 }
