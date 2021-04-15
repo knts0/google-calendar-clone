52b2a71a3882f68c87a77535e52cdfd417a49a9d
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-16 01:41:17 +0900

    remove useless code

--- src/app/page/modal/event-create/event-create.component.ts
+++ src/app/page/modal/event-create/event-create.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA }           from '@angular/material/dialog';
 import { FormControl, FormGroup }    from '@angular/forms';
 import * as dayjs                    from 'dayjs';
-import * as duration                 from 'dayjs/plugin/duration';
 
 export type EventCreateDialogData = {
   start: dayjs.Dayjs,
@@ -19,16 +18,9 @@ export class EventCreateComponent implements OnInit {
 
   form: FormGroup
 
-  isCalendarOpen = false
-
-  isTimePickerOpen = false
-
-  timeOptions: duration.Duration[]
-
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: EventCreateDialogData
   ) {
-    // dayjs.extend(duration)
     this.form = new FormGroup({
       title: new FormControl(''),
       startDate: new FormControl(this.data.start.format('YYYY-MM-DD')),
@@ -40,17 +32,6 @@ export class EventCreateComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const result = []
-    for (let minutes = 0; minutes < 60 * 24 / 15; minutes++) {
-      result.push(dayjs.duration(0, 'minutes').add(minutes * 15, 'minutes'))
-    }
-    this.timeOptions = result
-  }
-
-  onChangeStartDate($event: dayjs.Dayjs): void {
-    console.log($event)
-    this.form.patchValue({ date: $event })
-    this.isCalendarOpen = false
   }
 
 }
