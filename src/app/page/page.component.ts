d4bd494178adcdc2f6eb41809515076b27d13c25
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-16 01:59:57 +0900

    change event time

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -15,13 +15,13 @@ export class PageComponent implements OnInit {
   events: Event[] = [
     {
       title: '歯医者',
-      startTime: dayjs('2021-03-12 09:30'),
-      endTime: dayjs('2021-03-12 10:30')
+      startTime: dayjs('2021-04-12 09:30'),
+      endTime: dayjs('2021-04-12 10:30')
     },
     {
       title: 'ライブ',
-      startTime: dayjs('2021-03-09 19:00'),
-      endTime: dayjs('2021-03-09 19:30')
+      startTime: dayjs('2021-04-09 19:00'),
+      endTime: dayjs('2021-04-09 19:30')
     },
   ]
 
