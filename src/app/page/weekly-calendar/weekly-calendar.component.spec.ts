7cbbf9c6de81bd237909a52a64302592af4ccbdd
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-25 01:15:27 +0900

    add component test logic for weekly calendar component

--- src/app/page/weekly-calendar/weekly-calendar.component.spec.ts
+++ src/app/page/weekly-calendar/weekly-calendar.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import * as dayjs from 'dayjs';
+import { AppModule } from 'src/app/app.module';
 
 import { WeeklyCalendarComponent } from './weekly-calendar.component';
 
@@ -8,6 +11,7 @@ describe('WeeklyCalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ AppModule ],
       declarations: [ WeeklyCalendarComponent ]
     })
     .compileComponents();
@@ -16,10 +20,51 @@ describe('WeeklyCalendarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WeeklyCalendarComponent);
     component = fixture.componentInstance;
+
+    // input
+    component.activeDate = dayjs('2021-04-25').startOf('day')
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // activeDateを含む1週間の日付を正しく生成できるか
+  it('should calculate appropriate days in week', () => {
+    fixture.detectChanges()
+
+
+    const baseDate = dayjs('2021-04-19')
+    expect(component.days.map(v => v.day)).toEqual([
+      baseDate,
+      baseDate.add(1, 'day'),
+      baseDate.add(2, 'day'),
+      baseDate.add(3, 'day'),
+      baseDate.add(4, 'day'),
+      baseDate.add(5, 'day'),
+      baseDate.add(6, 'day'),
+    ])
+  })
+
+  // イベントの作成
+  it('should open event create dialog', fakeAsync(() => {
+    // observe onClick method
+    spyOn(component, 'onClickTimeFrame')
+
+    // emit click event
+    const cell = fixture.debugElement.query(By.css('.weekly-calendar-timeline__time-frame'))
+    cell.triggerEventHandler('click', null)
+    console.log(cell)
+
+    // emulate waiting for async event
+    tick()
+
+    // detect change
+    fixture.detectChanges()
+
+    //
+    expect(component.onClickTimeFrame).toHaveBeenCalled()
+  }))
 });
