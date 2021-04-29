de77405ab8321da6a4bfa0ff0b86d46becee0d25
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-29 23:02:55 +0900

    fix feature routing module

--- src/app/page/page-routing.module.ts
+++ src/app/page/page-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class PageRoutingModule { }
