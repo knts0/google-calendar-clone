156a6f619297fe7cc2fb3e2c917d7b57ff44d598
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-30 00:46:29 +0900

    use CalendarState in page component

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import * as dayjs            from 'dayjs';
+import { Component, OnInit }  from '@angular/core';
+import * as dayjs             from 'dayjs';
+import { Select, Store }      from '@ngxs/store';
+import { Emitter, Emittable } from '@ngxs-labs/emitter';
 
 import { Event }             from '../models/event';
 import { EventService } from '../services/event.service';
+import { CalendarState } from './state/calendar.state';
+import { Observable } from 'rxjs';
 
 export type CalendarViewMode = 'month' | 'week'
 
@@ -13,35 +17,49 @@ export type CalendarViewMode = 'month' | 'week'
 })
 export class PageComponent implements OnInit {
 
-  activeDate: dayjs.Dayjs = dayjs().startOf('day')
+  @Select(CalendarState.activeDate) activeDate$: Observable<dayjs.Dayjs>;
+
+  @Emitter(CalendarState.setActiveDate)
+  private setActiveDateEmitter: Emittable<dayjs.Dayjs>
 
   today: dayjs.Dayjs = dayjs().startOf('day')
 
   calendarViewMode: CalendarViewMode = 'week'
 
-  constructor(private eventService: EventService) { }
+  constructor(
+    private eventService: EventService,
+    private store: Store,
+  ) { }
 
   ngOnInit(): void {
     this.eventService.test().subscribe(_ => console.log('test'))
   }
 
   onChangeActiveDate(date: dayjs.Dayjs) {
-    this.activeDate = date
+    this.setActiveDateEmitter.emit(date)
   }
 
   changeActiveDatePrev(): void {
     switch (this.calendarViewMode) {
-      case 'week': this.activeDate = this.activeDate.subtract(1, 'week')
+      case 'week':
+        this.setActiveDateEmitter.emit(
+          this.store.selectSnapshot(CalendarState.activeDate).subtract(1, 'week')
+        )
     }
   }
 
   changeActiveDateNext(): void {
     switch (this.calendarViewMode) {
-      case 'week': this.activeDate = this.activeDate.add(1, 'week')
+      case 'week':
+        this.setActiveDateEmitter.emit(
+          this.store.selectSnapshot(CalendarState.activeDate).add(1, 'week')
+        )
     }
   }
 
   changeActiveDateToToday(): void {
-    this.activeDate = dayjs().startOf('day')
+    this.setActiveDateEmitter.emit(
+      dayjs().startOf('day')
+    )
   }
 }
