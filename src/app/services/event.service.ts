578856132927bb07e270e59ade79487055679f0a
Kana Otawara <shimmer22.0204@gmail.com>
2021-05-09 01:40:12 +0900

    create getEventsInAWeek in event service

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -3,11 +3,16 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { map }        from 'rxjs/operators';
+import { Event } from '../models/event';
 import { NewEvent } from '../models/new-event';
+import * as dayjs from 'dayjs';
+import { EventResponse, EventResponseModule } from './event-response';
 
 const TEST_URL = 'api/test'
 
 const CREATE_EVENT_URL = 'api/event'
+
+const GET_EVENT_IN_A_WEEK = 'api/event/week'
 
 @Injectable({
   providedIn: 'root'
@@ -26,5 +31,16 @@ export class EventService {
 
   createEvent(event: NewEvent): Observable<void> {
     return this.http.post<void>(CREATE_EVENT_URL, event)
+  }
+
+
+  getEventsInAWeek(weekStartDate: dayjs.Dayjs): Observable<Event[]> {
+    return this.http.get<EventResponse>(
+      GET_EVENT_IN_A_WEEK
+      + '?weekStartDate='
+      + weekStartDate.format('YYYY-MM-DD')
+    ).pipe(
+      map((res: EventResponse) => EventResponseModule.toModel(res))
+    )
   }
 }
