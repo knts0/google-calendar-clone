4b3435e61c36e9d7a08a72533593fb4528d8c3ee
Kana Otawara <shimmer22.0204@gmail.com>
2021-05-09 01:40:56 +0900

    define events in calendar state

--- src/app/page/state/calendar.state.ts
+++ src/app/page/state/calendar.state.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Action, Selector, State, StateContext } from '@ngxs/store';
 import * as dayjs from 'dayjs';
+import { tap } from 'rxjs/operators';
+import { Event } from '../../models/event';
+import { EventService } from 'src/app/services/event.service';
 import { CalendarActions } from './calendar.actions';
 
 export type CalendarViewMode = 'month' | 'week'
 
+const FIRST_DAY_OF_WEEK = 1
+
 export interface CalendarStateModel {
   activeDate: dayjs.Dayjs,
   calendarViewMode: CalendarViewMode,
+  events: Event[],
 }
 
 
@@ -16,10 +22,15 @@ export interface CalendarStateModel {
   defaults: {
     activeDate: dayjs().startOf('day'),
     calendarViewMode: 'week',
+    events: [],
   }
 })
 @Injectable()
 export class CalendarState {
+
+  constructor(
+    private eventService: EventService
+  ) {}
 
   @Selector()
   static activeDate(state: CalendarStateModel): dayjs.Dayjs {
@@ -29,6 +40,11 @@ export class CalendarState {
   @Selector()
   static calendarViewMode(state: CalendarStateModel): CalendarViewMode {
     return state.calendarViewMode
+  }
+
+  @Selector()
+  static events(state: CalendarStateModel): Event[] {
+    return state.events
   }
 
   @Action(CalendarActions.SetActiveDateAction)
@@ -46,34 +62,50 @@ export class CalendarState {
   setActiveDateToPrev(
     ctx: StateContext<CalendarStateModel>,
   ) {
-    const state = ctx.getState();
-
-    let newDate;
+    const state = ctx.getState()
     switch (state.calendarViewMode) {
-      case 'week':
-        newDate = state.activeDate.subtract(1, 'week')
-        break
+      case 'week': {
+        const newDate = state.activeDate.subtract(1, 'week')
+        const weekStartDate = this.getFirstDayOfWeek(newDate)
+        return this.eventService.getEventsInAWeek(weekStartDate)
+          .pipe(
+            tap((events: Event[]) =>
+              ctx.patchState({
+                activeDate: newDate,
+                events: events,
+              })
+            )
+          )
+      }
     }
-    ctx.patchState({
-      activeDate: newDate
-    });
   }
 
   @Action(CalendarActions.SetActiveDateToNext)
   setActiveDateToNext(
     ctx: StateContext<CalendarStateModel>,
   ) {
-    const state = ctx.getState();
-
-    let newDate;
+    const state = ctx.getState()
     switch (state.calendarViewMode) {
-      case 'week':
-        newDate = state.activeDate.add(1, 'week')
-        break
+      case 'week': {
+        const newDate = state.activeDate.add(1, 'week')
+        const weekStartDate = this.getFirstDayOfWeek(newDate)
+        return this.eventService.getEventsInAWeek(weekStartDate)
+          .pipe(
+            tap((events: Event[]) =>
+              ctx.patchState({
+                activeDate: newDate,
+                events: events,
+              })
+            )
+          )
+      }
     }
-    ctx.patchState({
-      activeDate: newDate
-    });
+  }
+
+  private getFirstDayOfWeek(date: dayjs.Dayjs): dayjs.Dayjs {
+    return (date.day() < FIRST_DAY_OF_WEEK)
+      ? date.subtract(1, 'week').day(FIRST_DAY_OF_WEEK)
+      : date.day(FIRST_DAY_OF_WEEK)
   }
 
   @Action(CalendarActions.SetActiveDateToTodayAction)
