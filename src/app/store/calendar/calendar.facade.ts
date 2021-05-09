65a8eea0fdcbc775397fc2169b95f40aa3501111
Kana Otawara <shimmer22.0204@gmail.com>
2021-05-09 20:11:28 +0900

    create calendar facade

---
+++ src/app/store/calendar/calendar.facade.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import * as dayjs from 'dayjs';
+import { Observable } from 'rxjs';
+
+import { CalendarViewMode } from '../../models/calendar-view-mode';
+import { Event } from '../../models/event';
+import { CalendarActions } from './calendar.actions';
+import { CalendarState } from './calendar.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalendarFacade {
+  @Select(CalendarState.activeDate) activeDate$: Observable<dayjs.Dayjs>;
+  @Select(CalendarState.calendarViewMode) calendarViewMode$: Observable<CalendarViewMode>;
+  @Select(CalendarState.events) events$: Observable<Event[]>;
+
+  constructor(
+    private store: Store
+  ) {}
+
+  loadEvents(): void {
+    this.store.dispatch(new CalendarActions.LoadEvents())
+  }
+
+  setActiveDate(date: dayjs.Dayjs): void {
+    this.store.dispatch(new CalendarActions.SetActiveDateAction(date))
+  }
+
+  setActiveDateToPrev(): void {
+    this.store.dispatch(new CalendarActions.SetActiveDateToPrev())
+  }
+
+  setActiveDateToNext(): void {
+    this.store.dispatch(new CalendarActions.SetActiveDateToPrev())
+  }
+
+  setActiveDateToToday(): void {
+    this.store.dispatch(new CalendarActions.SetActiveDateAction(dayjs().startOf('day')))
+  }
+}
