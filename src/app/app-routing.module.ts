fc07d9fff709c3b573b047599c9924289b4ee729
Kana Otawara <shimmer22.0204@gmail.com>
2021-05-09 21:45:33 +0900

    change directory name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AngularComponent }     from './angular.component';
 
 const routes: Routes = [
   { path: 'angular', component: AngularComponent },
-  { path: 'top', loadChildren: () => import('./page/page.module').then(m => m.PageModule) },
+  { path: 'top', loadChildren: () => import('./pages/page.module').then(m => m.PageModule) },
   { path: 'test', loadChildren: () => import('./test/test.module').then(m => m.TestModule) },
 ];
 
