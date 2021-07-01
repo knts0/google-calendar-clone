3e463a5d376fd8100d30b73df8a1725478749f39
Kana Otawara <shimmer22.0204@gmail.com>
2021-07-02 00:26:22 +0900

    implement calendar view mode selector

--- src/app/pages/top/components/header/header.component.ts
+++ src/app/pages/top/components/header/header.component.ts
@@ -6,7 +6,9 @@ import {
   OnInit,
   Output
 } from '@angular/core'
+import { FormControl } from '@angular/forms'
 import * as dayjs from 'dayjs'
+import { CalendarViewMode } from '../../containers/top/top.container'
 
 @Component({
   selector: 'app-header',
@@ -21,6 +23,12 @@ export class HeaderComponent implements OnInit {
   @Output() onPrevClicked:  EventEmitter<void> = new EventEmitter()
   @Output() onNextClicked:  EventEmitter<void> = new EventEmitter()
   @Output() onTodayClicked: EventEmitter<void> = new EventEmitter()
+
+  calendarViewModeOptions: { value: CalendarViewMode, label: string }[] = [
+    { value: 'month', label: '月' },
+    { value: 'week', label: '週' },
+  ]
+  calendarViewModeControl = new FormControl('month')
 
   constructor(
   ) { }
